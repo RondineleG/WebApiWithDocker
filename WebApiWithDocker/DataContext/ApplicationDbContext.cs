33fca7822d3ab6b0bc70ee6ffc384788caf6e273
Rondinele Guimaraes <rondineleg@outlook.com>
2022-09-04 18:17:47 -0300

    API com docker e sql server

---
+++ WebApiWithDocker/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using WebApiWithDocker.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiWithDocker.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
